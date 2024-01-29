eb8845466c00b004a1d463b747e3da0737f59655
Strzegomiak <lukaszjak@gmail.com>
2024-01-29 16:45:01 +0100

    29.01

--- job-app/src/components/JobsPanel.tsx
+++ job-app/src/components/JobsPanel.tsx
@@ -9,13 +9,19 @@ const JobsPanel: React.FC<JobsPanelProps> = (props) => {
   return (
     <ul className="flex justify-center w-4/5 flex-auto gap-14 mt-10  flex-wrap">
       {props.copyOfJobOffers ? (
-        props.copyOfJobOffers.map((singleJob) => (
-          <li key={singleJob.id}>
-            <OneJobPanel singleJob={singleJob} />
-          </li>
-        ))
+        props.copyOfJobOffers.length > 0 ? (
+          props.copyOfJobOffers.map((singleJob) => (
+            <li key={singleJob.id}>
+              <OneJobPanel singleJob={singleJob} />
+            </li>
+          ))
+        ) : (
+          <h2 className="text-4xl mt-20 text-red-500">
+            No offers for the given filters
+          </h2>
+        )
       ) : (
-        <li>No job offers available</li>
+        <li className="text-4xl mt-20 text-red-500">No job offers available</li>
       )}
     </ul>
   );
